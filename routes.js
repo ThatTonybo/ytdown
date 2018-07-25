a4e7f37fed9483aebd56f8e728a81385be4157ed
ThatTonybo <twatt74@eq.edu.au>
2018-07-25 16:15:26 +1000

    add random ids plus new package

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 const fs = require('fs')
     , ytdl = require('ytdl-core')
     , path = require('path')
-    , ffmpeg   = require('fluent-ffmpeg');
+    , ffmpeg   = require('fluent-ffmpeg')
+    , rs = require('randomstring');
 
 router.get('/', async (req, res) => {
     res.render('index', { error: null });
@@ -26,12 +27,13 @@ router
             video_id = video_url.split('be/')[1];
         }
 
+        const code = `${rs.generate(12)}`;
         if (format_chosen === 'mp4') {
             const s = ytdl(`${video_url}`, { filter: (format) => format.container === `mp4` })
-            .pipe(fs.createWriteStream(path.resolve(`download.mp4`)));
+            .pipe(fs.createWriteStream(path.resolve(`${code}.mp4`)));
 
             s.on('end', () => {
-                res.redirect('/download/download.mp4');
+                res.redirect(`/download/${code}.mp4`);
             });
         }
         if (format_chosen === 'mp3') {
@@ -39,9 +41,9 @@ router
 
             ffmpeg(stream)
                 .audioBitrate(128)
-                .save(`${__dirname}/download.mp3`)
+                .save(`${__dirname}/${code}.mp3`)
                 .on('end', () => {
-                    res.redirect('/download/download.mp3');
+                    res.redirect(`/download/${code}.mp3`);
                 });
         }
 
